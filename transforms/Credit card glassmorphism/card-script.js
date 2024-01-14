63718120b1b1886d7449da8958ef8a22f04f97bb
GartyCoding <155091105+GartyCoding@users.noreply.github.com>
2024-01-14 15:02:15 +0700

    remove js

--- transforms/Credit card glassmorphism/card-script.js
+++ transforms/Credit card glassmorphism/card-script.js
@@ -13,17 +13,3 @@ Array.from(cardNum).forEach(function(element, i) {
         }
     })
 });
-
-const valid = document.querySelectorAll(".valid");
-Array.from(valid).forEach(function(element, i) {
-    element.addEventListener("keyup", (e) => {
-        if (e.isComposing || e.keyCode === 229) {
-            return;
-        }
-
-        const count = element.value.length;
-        if (count === 2 && (i + 1) < count) {
-            valid[i+1].focus();
-        }
-    });
-});
